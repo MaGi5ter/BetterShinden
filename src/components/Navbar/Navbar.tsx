8a4bb78b54338ff72049aa3eb38b11acc0bd32cb
MaGi5ter <xvxjacekxvx@gmail.com>
2023-06-03 21:31:50 +0200

    navbar fixes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,53 @@
 import React from "react";
 import { useState } from "react";
+import { fetchRAW } from "../../assets/scripts/dataFetchScripts";
 import Popup from "../LoginPopup/popup";
 import "./navbar.css";
+
+async function checkLogged() {
+  let mainPage = await fetchRAW("https://shinden.pl");
+  const parser = new DOMParser();
+  const document = parser.parseFromString(mainPage, "text/html");
+
+  let test = document.querySelector(".top-bar--nav-user");
+  console.log(test);
+  if (test == null) {
+    return {
+      logged: false,
+      logout_link: "",
+    };
+  } else {
+    let logout_link: string;
+    test
+      .querySelector(".bd")
+      ?.querySelectorAll("li")
+      .forEach((element, index) => {
+        if (index == 3) {
+          logout_link = element.querySelector("a")?.href!;
+        }
+      });
+
+    return {
+      logged: true,
+      logout_link: logout_link!,
+    };
+  }
+}
 
 function Navbar() {
   const [data, setData] = useState<animeData[] | []>();
   const [loading, setLoading] = useState(true);
   const [showPopup, setShowPopup] = useState(false);
+  const [logged, setLogged] = useState({
+    logged: false,
+    logout_link: "",
+  });
+  const [checkinglogged, setCheckingLogged] = useState(true);
 
+  if (checkinglogged) {
+    setCheckingLogged(false);
+    handleLogged();
+  }
   interface animeData {
     title_id: string;
     title: string;
@@ -18,6 +58,12 @@ function Navbar() {
     premiere_date: string;
     other_to: string;
     title_url: string;
+  }
+
+  async function handleLogged() {
+    let data = await checkLogged();
+    console.log(data);
+    setLogged(data);
   }
 
   function redirect(link: string) {
@@ -65,54 +111,60 @@ function Navbar() {
     <>
       <Popup trigger={showPopup} close={handlePopup}></Popup>
       <div id="navbar">
-        <a href="/" className="logo_link">
-          <h1 id="logo"> BetterShinden</h1>
-        </a>
-        <div id="searchbar">
-          <div className="srch">
-            <input
-              className="searc__bar"
-              type="text"
-              placeholder="Szukaj Anime.."
-              onChange={fetchAnimes}
-            />
-            <button type="submit">
-              <i className="fa fa-search"></i>
-            </button>
-          </div>
-          <ul
-            className="drop"
-            style={{
-              display: loading ? "none" : "block",
-            }}
-          >
-            {loading
-              ? ""
-              : data?.slice(0, 7).map((search_result) => {
-                  if (search_result.type === "Anime") {
-                    return (
-                      <li
-                        className="title_result"
-                        onClick={() => redirect(search_result.title_url)}
-                      >
-                        <img
-                          src={`/res/images/225x350/${search_result.cover}.jpg`}
-                          alt="COVER"
-                        />
-                        <div className="title_content">
-                          <h2>{search_result.title}</h2>
-                          <h3>{search_result.title_status}</h3>
-                        </div>
-                      </li>
-                    );
-                  }
-                })}
-          </ul>
-        </div>
-        <div id="nav__buttons">
-          <a href="#">
-            <button onClick={(e) => handlePopup()}>Logowanie</button>
+        <div className="navbar-content">
+          <a href="/" className="logo_link">
+            <h1 id="logo"> BetterShinden</h1>
           </a>
+          <div id="searchbar">
+            <div className="srch">
+              <input
+                className="searc__bar"
+                type="text"
+                placeholder="Szukaj Anime.."
+                onChange={fetchAnimes}
+              />
+              <button type="submit">
+                <i className="fa fa-search"></i>
+              </button>
+            </div>
+            <ul
+              className="drop"
+              style={{
+                display: loading ? "none" : "block",
+              }}
+            >
+              {loading
+                ? ""
+                : data?.slice(0, 7).map((search_result) => {
+                    if (search_result.type === "Anime") {
+                      return (
+                        <li
+                          className="title_result"
+                          onClick={() => redirect(search_result.title_url)}
+                        >
+                          <img
+                            src={`/res/images/225x350/${search_result.cover}.jpg`}
+                            alt="COVER"
+                          />
+                          <div className="title_content">
+                            <h2>{search_result.title}</h2>
+                            <h3>{search_result.title_status}</h3>
+                          </div>
+                        </li>
+                      );
+                    }
+                  })}
+            </ul>
+          </div>
+          <div id="nav__buttons">
+            <a href={logged.logged ? logged.logout_link : "#"}>
+              {logged.logged ? (
+                <button>Wyloguj</button>
+              ) : (
+                <button onClick={(e) => handlePopup()}>Logowanie</button>
+              )}
+            </a>
+          </div>
         </div>
       </div>
     </>
